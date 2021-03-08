f3ba5e03729e5c7c66fe9864c577ccdd73eb156e
Mohammed0812 <79253151+Mohammed0812@users.noreply.github.com>
2021-03-08 16:46:39 +0530

    Update report.component.ts

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -7,8 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportComponent implements OnInit {
 
-  constructor() { }
-
+  fname:any = '';
+  fprice:any; 
+  products:any = [];
+transaction:any = [];
+  rname:any;
+  sum:any = 0;
+  total:any = 1;
+  reports:any = [];
+  constructor() {
+    
+    let a = localStorage.getItem('products');
+    if (a) {
+      this.products = JSON.parse(a);
+    }
+   }
+  addReport() {
+    let c = {
+      Name:this.rname,
+      totals:this.total
+    }
+    this.reports.push(c);
+    this.sum = this.reports.reduce((a: number, b: any) => {
+      return a + parseInt(b.totals);
+    }, 0);
+  }
   ngOnInit(): void {
   }
 
