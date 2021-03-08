e77cee370a3ef5683afa9ba89eedc83d0e567aa3
Mohammed0812 <79253151+Mohammed0812@users.noreply.github.com>
2021-03-08 16:47:03 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- 
+  fname:any = '';
+  fprice:any; 
+  products:any = [];
+  constructor() {
+    let a = localStorage.getItem('products');
+    if (a) {
+      this.products = JSON.parse(a);
+    }
+   }
+  addProduct() {
+    let p = {
+      name: this.fname,
+      price: this.fprice
+    }
+    this.products.push(p);
+    localStorage.setItem('products', JSON.stringify(this.products));
+  }
+  delete(i: any) {
+    this.products.splice(i, 1);
+  }
+  images: any;
+  image () {
+    this.images.style.display = 'none';
+  }
 }
